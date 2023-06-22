fa1029c07cc4ca79d3bb1bcbea615a96ab7f3544
RaccoonGem <jack@honeydog.org>
2023-06-22 18:39:22 -0500

    Initial Commit

---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+const axios = require('axios');
+
+const App = () => {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
