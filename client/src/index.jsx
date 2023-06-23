49b6bb24893d5350afed7e8fa4796119173bcec8
darienpoon <darienpoon1@gmail.com>
2023-06-22 17:24:59 -0700

    placeholder for related-items

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import RelatedItems from './components/relatedItems/RelatedItems.jsx';
 
 const axios = require('axios');
 
@@ -7,7 +8,7 @@ const App = () => {
 
   return (
     <div>
-
+      <RelatedItems />
     </div>
   );
 }
