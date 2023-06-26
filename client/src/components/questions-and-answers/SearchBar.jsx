1da6d4e2d0a177726ef420bfee809ab21706feba
Yichen <chenlin7789@gmail.com>
2023-06-26 13:31:54 -0700

    added the layout for the SearchBar component

--- client/src/components/questions-and-answers/SearchBar.jsx
+++ client/src/components/questions-and-answers/SearchBar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = () => {
   return (
-    <>
-      <div>Search Bar</div>
-    </>
+    <div className='Search'>
+      <div className='searchInputs'>
+        <input
+          type='text'
+          placeholder='HAVE A QUESTION? SEARCH FOR ANSWER...'
+        />
+        <div className='searchIcon'>
+          <FaSearch />
+        </div>
+      </div>
+      <div className='dataResult'></div>
+    </div>
   )
 };
 
