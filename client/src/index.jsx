dcb1ba545830658a0046ed5a4150231af73b0c7c
jddg5wa <2061754+jddg5wa@users.noreply.github.com>
2023-06-27 11:46:29 -0700

    Added average current product rating useState and value. Refactored all the axios calls into one axios.all()

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,37 +7,55 @@ import RelatedItems from './components/RelatedItems.jsx';
 
 const axios = require('axios');
 
+const getAvgRating = function (ratings) {
+  let count = 0;
+  let result = 0;
+  for (let f = 1; f <= 5; f++) {
+    let rating = Number(ratings[f])
+    count += rating;
+    result += rating * f;
+  }
+  if (count > 0) {
+    result /= count;
+    result = (Math.round(result * 100) / 100);
+  }
+  return result;
+}
+
 const App = () => {
   const [products, setProducts] = useState([]);
   const [currentProduct, setCurrentProduct] = useState({});
-  const [productStyles, setProductStyles] = useState({}); // product styles of current product
-  const [relatedProducts, setRelatedProducts] = useState([]); // related products of current products
+  const [currentProductStyles, setCurrentProductStyles] = useState({}); // product styles of current product
+  const [currentRelatedProducts, setCurrentRelatedProducts] = useState([]); // related products of current product
+  const [currentProductAvgRating, setCurrentAvgRating] = useState(0); // average rating of current product
 
-  var axiosHeaders = {headers:{"Authorization" : process.env.AUTH_SECRET}};
 
   useEffect(() => {
-    // call for all products to get initial product ID for later calls
+    var axiosHeaders = {headers:{"Authorization" : process.env.AUTH_SECRET}};
+
+    //get all products to get initial product ID for current product
     axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/${process.env.CAMPUS}/products/`, axiosHeaders)
     .then ((response) => {
       setProducts(response.data);
       setCurrentProduct(response.data[0]);
-      return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/${process.env.CAMPUS}/products/${response.data[0].id}/styles`, axiosHeaders);
+
+      var endpoints = [
+        `https://app-hrsei-api.herokuapp.com/api/fec2/${process.env.CAMPUS}/products/${response.data[0].id}/styles`,
+        `https://app-hrsei-api.herokuapp.com/api/fec2/${process.env.CAMPUS}/reviews/meta?product_id=${response.data[0].id}`,
+        `https://app-hrsei-api.herokuapp.com/api/fec2/${process.env.CAMPUS}/products/${response.data[0].id}/related`,
+      ];
+
+      return axios.all(endpoints.map((endpoint) => axios.get(endpoint, axiosHeaders)));
     })
-    .then((response) => {
-      setProductStyles(response.data.results);
-      return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/${process.env.CAMPUS}/products/${response.data.product_id}/related`, axiosHeaders);
-    })
-    .then ((response) => {
-      setRelatedProducts(response.data);
-    })
+    .then(axios.spread(({data: styles}, {data: reviews}, {data: related}) => {
+      setCurrentProductStyles(styles);
+      setCurrentRelatedProducts(related);
+      setCurrentAvgRating(getAvgRating(reviews.ratings));
+    }))
     .catch ((error) => {
       throw error;
     });
   }, []);
-
-  console.log(currentProduct);
-  console.log(productStyles);
-  console.log(relatedProducts);
 
   return (
     <div>
@@ -49,19 +67,6 @@ const App = () => {
   );
 }
 
-const getAvgRating = function (ratings) {
-  let count = 0;
-  let result = 0;
-  for (let f = 1; f <= 5; f++) {
-    count += ratings[f];
-    result += ratings[f] * f;
-  }
-  if (count > 0) {
-    result /= count;
-    result = (Math.round(result * 100) / 100);
-  }
-  return result;
-}
 
 ReactDOM.render(
   <App />,
