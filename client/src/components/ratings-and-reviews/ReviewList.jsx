0c154b9b9b90847f451d1426726b4f1155ed9b76
RaccoonGem <jack@honeydog.org>
2023-07-07 16:35:00 -0500

    Add more features to review list

--- client/src/components/ratings-and-reviews/ReviewList.jsx
+++ client/src/components/ratings-and-reviews/ReviewList.jsx
@@ -5,9 +5,11 @@ const ReviewList = (props) => {
 
   return (<div role='review-list'>
     <h4>Reviews</h4>
-    {props.reviews.results.map((r) => (
-      <Review review={r} key={r.review_id}/>
-    ))}
+    <div className='review-list-content'>
+      {props.reviews.results.slice(0, props.maxReviews).map((r) => (
+        <Review review={r} key={r.review_id}/>
+      ))}
+    </div>
   </div>);
 }
 
